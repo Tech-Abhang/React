ef20073fa6bf4ca6905153cc940fad4d597a4ef4
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-21 21:37:40 +0530

    styling components

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 
 //import
-import Title from "./Title.jsx"
-import Product from "./Product.jsx"
 import ProductTab from "./ProductTab.jsx"
 
 function App() {
   return (  
-    <ProductTab/>
+    <ProductTab></ProductTab>
   )
 }
 
