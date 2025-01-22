fb4a4f1d1945d7add3fc1db50676a14da5fb2674
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-22 11:10:50 +0530

    added propmts

--- react-app/src/ProductTab.jsx
+++ react-app/src/ProductTab.jsx
@@ -3,9 +3,9 @@ import Product from "./Product.jsx"
 function ProductTab(){
     return(
     <>
-        <Product/>
-        <Product/>
-        <Product/>
+        <Product title ="laptop" price={100000}/>
+        <Product title="phone"price={100000}/>
+        <Product title="pen"/>
     </>
     )
 }
